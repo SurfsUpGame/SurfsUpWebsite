4bce1dae6b70b16227e00f5b3636c66e42b4d61a
Mark <markarneman@gmail.com>
2025-06-16 09:58:21 -0400

    Trusted Proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: [
+            '192.168.1.1',
+            '10.0.0.0/8',
+            '172.16.0.0/16'
+        ]);
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
-        cors: false,
+        cors: true,
     },
 });
