ee65c7a0185f88c7ce605a3c50e0abdb85084c09
Mark <markarneman@gmail.com>
2025-06-16 09:45:00 -0400

    Reset to vite

--- resources/views/public-roadmap.blade.php
+++ resources/views/public-roadmap.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>SurfsUp Roadmap</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite('resources/css/app.css') {{-- or whatever CSS your app uses --}}
     @livewireStyles
     @filamentStyles
 </head>
