5766686bf33170e3fd41646d6dde4af68e71914f
Mark <markarneman@gmail.com>
2025-06-16 08:48:34 -0400

    Removed registering routes

--- routes/web.php
+++ routes/web.php
@@ -8,23 +8,21 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('login', \App\Http\Controllers\Auth\SteamAuthController::class);
+//Route::view('dashboard', 'dashboard')
+//    ->middleware(['auth', 'verified'])
+//    ->name('dashboard');
+//
+//Route::middleware(['auth'])->group(function () {
+//    Route::redirect('settings', 'settings/profile');
+//
+//    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+//    Volt::route('settings/password', 'settings.password')->name('settings.password');
+//    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+//});
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
-
-Route::get('login', \App\Http\Controllers\Auth\SteamAuthController::class)->name('login');
+// Route::get('login', \App\Http\Controllers\Auth\SteamAuthController::class)->name('login');
 Route::get('/roadmap', function () {
     return view('public-roadmap');
 })->name('roadmap');
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
