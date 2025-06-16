e553d12a6cf887123aaa99aaf08a60358d18cc8b
Mark <markarneman@gmail.com>
2025-06-16 10:01:12 -0400

    Trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,12 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
             '10.0.0.0/8',
             '172.16.0.0/16'
         ]);
-        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
-            Request::HEADER_X_FORWARDED_HOST |
-            Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO |
-            Request::HEADER_X_FORWARDED_AWS_ELB
-        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
