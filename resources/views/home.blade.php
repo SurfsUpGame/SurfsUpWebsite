7052c79a998c8ef2dac62204072c3f32d5e7ec5b
Mark <markarneman@gmail.com>
2025-07-03 01:49:29 -0400

    rybbit analytics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,14 @@
     <title>SurfsUp - Free-to-Play High Speed Multiplayer Precision Platformer</title>
     <meta name="description" content="SurfsUp is a free-to-play high speed multiplayer precision platformer. Wishlist now on Steam!">
     <meta name="keywords" content="SurfsUp, platformer, multiplayer, free to play, steam, indie game">
-    
+
     <link rel="icon" type="image/x-icon" href="{{ asset('/img/favicon.ico') }}">
-    
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    
+
     <script src="https://kit.fontawesome.com/d251d3e9b0.js" crossorigin="anonymous"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
-    
+
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-H68DQ85G4C"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -21,20 +21,26 @@
         gtag('js', new Date());
         gtag('config', 'G-H68DQ85G4C');
     </script>
+
+    <script
+        src="https://app.rybbit.io/api/script.js"
+        data-site-id="1407"
+        defer
+    ></script>
 </head>
 <body class="bg-gray-900 text-white antialiased">
     @include('partials.header')
-    
+
     <main>
         @include('partials.hero')
-        
+
         @include('partials.live-streams')
-        
+
         @include('partials.media-section')
-        
+
         @include('partials.survivalscape-promo')
     </main>
-    
+
     @include('partials.footer')
 </body>
 </html>
