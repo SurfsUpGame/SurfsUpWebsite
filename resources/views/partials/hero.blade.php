bb2f126dc817109566d29b192510e6b3607ceaa3
Mark <markarneman@gmail.com>
2025-07-04 18:59:44 -0400

    no longer target blank roadmap

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -29,7 +29,6 @@
                 </a>
 
                 <a href="{{ route('roadmap') }}"
-                   target="_blank"
                    class="group relative inline-flex items-center gap-3 bg-orange-600 hover:bg-orange-700 px-8 py-4 rounded-lg text-white font-medium transition-all duration-200 transform hover:scale-105 hover:shadow-xl">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
